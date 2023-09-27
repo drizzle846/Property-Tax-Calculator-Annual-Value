6a124fc51bfd625d83cf265aaa80707eb116b72a
drizzle846 <125164693+drizzle846@users.noreply.github.com>
2023-09-26 23:40:49 -0700

    Add files via upload

---
+++ Calculator2/src/main/java/com/example/PropertyTaxCalculator/PropertyTaxCalculator.java
@@ -0,0 +1,13 @@
+package com.example.PropertyTaxCalculator;
+
+public class PropertyTaxCalculator {
+    public static double calculateProprtyTax(double UC, double UVpsm, double UAP, double Age, double taxRate, double SF, double OF) {
+       
+         
+ 
+        return UC * UVpsm * UAP  * Age * taxRate * SF * OF;}
+
+    
+}
+
+
